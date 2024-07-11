fcffc5279a2dd7ff569d033e8d3b11f321d81330
ShaharAshe <shaharas30@gmail.com>
2024-07-11 16:28:03 +0300

    del square_and_mul function

--- square_and_mul.py
+++ square_and_mul.py
@@ -1,28 +1,28 @@
-def square_and_mul(c, z):
-    c_bit = []
-    temp_c = c
-    s_k = []
-    r_k = []
+# def square_and_mul(c, z):
+#     c_bit = []
+#     temp_c = c
+#     s_k = []
+#     r_k = []
 
-    while temp_c != 0:
-        if temp_c % 2 == 0:
-            c_bit.insert(0, 0)
-        else:
-            c_bit.insert(0, 1)
-        temp_c >>= 1
-    print(f"{c} in binary is {c_bit}")
+#     while temp_c != 0:
+#         if temp_c % 2 == 0:
+#             c_bit.insert(0, 0)
+#         else:
+#             c_bit.insert(0, 1)
+#         temp_c >>= 1
+#     print(f"{c} in binary is {c_bit}")
 
-    k = 0
-    for i in range(len(c_bit)):
-        s_k.append((c_bit[i]**k)%self.n)
-        if k == 0:
-            k += 2
-        if i == 0:
-            r_k.append()
-        else:
-            r_k.append((s_k[-1]*z)%self.n)
+#     k = 0
+#     for i in range(len(c_bit)):
+#         s_k.append((c_bit[i]**k)%self.n)
+#         if k == 0:
+#             k += 2
+#         if i == 0:
+#             r_k.append()
+#         else:
+#             r_k.append((s_k[-1]*z)%self.n)
             
 
 
-if __name__ == "__main__":
-    square_and_mul(8, 5658)
+# if __name__ == "__main__":
+#     square_and_mul(8, 5658)
