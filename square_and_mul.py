e66dfed0a600ba1a532abc9eedb3a44e5b9bd3e5
ShaharAshe <shaharas30@gmail.com>
2024-07-12 13:19:25 +0300

    square_and_mul

--- square_and_mul.py
+++ square_and_mul.py
@@ -1,28 +1,47 @@
-# def square_and_mul(c, z):
-#     c_bit = []
-#     temp_c = c
-#     s_k = []
-#     r_k = []
+def square_and_mul(n, c, z, ts, c_ab):
+        c_bit = []
+        temp_c = c
+        s_k = []
+        r_k = []
 
-#     while temp_c != 0:
-#         if temp_c % 2 == 0:
-#             c_bit.insert(0, 0)
-#         else:
-#             c_bit.insert(0, 1)
-#         temp_c >>= 1
-#     print(f"{c} in binary is {c_bit}")
+        while temp_c != 0:
+            if temp_c % 2 == 0:
+                c_bit.insert(0, 0)
+            else:
+                c_bit.insert(0, 1)
+            temp_c >>= 1
+        print(f"{c} in binary is {c_bit}")
 
-#     k = 0
-#     for i in range(len(c_bit)):
-#         s_k.append((c_bit[i]**k)%self.n)
-#         if k == 0:
-#             k += 2
-#         if i == 0:
-#             r_k.append()
-#         else:
-#             r_k.append((s_k[-1]*z)%self.n)
+        k = 0
+        z_new = 1
+        for i in range(len(c_bit)):
+            print("-----------------------------")
+            print(f'{i = }')
+            print(f'e_i = {c_bit[i]}')
             
+            if k == 0:
+                s_k.append(1)
+                k += 2
+                print(f'z^2 = 1 (mod {n})')
+            else:
+                z_new = (z_new**2)%n
+                s_k.append(z_new)
+                print(f'z^2 = {s_k[-2]}^2 = {s_k[-1]} (mod {n})')
+
+            if c_bit[i] == 1:
+                z_new = (z_new*z)%n
+                r_k.append(z_new)
+                print(f'z*{z} = {z_new}*{z} = {r_k[-1]} (mod {n})')
+        print("-----------------------------")
+        print(f'And we got that {c_ab}^{ts} = {z_new} (mod {n})')
+        return z_new
 
 
-# if __name__ == "__main__":
-#     square_and_mul(8, 5658)
+if __name__ == "__main__":
+    n = 16157
+    # c = 2
+    # z = 13910
+    power = 2
+    base = 13910
+
+    square_and_mul(n, power, base, power, base)
