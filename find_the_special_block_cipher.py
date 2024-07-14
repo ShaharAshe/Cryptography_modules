baa7c756cf904ab0d30f8f4320794d388afcc1a9
ShaharAshe <shaharas30@gmail.com>
2024-07-14 15:27:53 +0300

    fix find_the_special_block_cipher

--- find_the_special_block_cipher.py
+++ find_the_special_block_cipher.py
@@ -1,16 +1,17 @@
-def find_the_special_block_cipher(M1, M2, M3):
-    M1 = M1.replace(" ", "")
-    M2 = M2.replace(" ", "")
-    M3 = M3.replace(" ", "")
+def find_the_special_block_cipher(M1:str, M2:str, M3:str) -> str:
+    M1:str = M1.replace(" ", "")
+    M2:str = M2.replace(" ", "")
+    M3:str = M3.replace(" ", "")
 
     if len(M1) != len(M2) or len(M1) != len(M3) or len(M2) != len(M3):
         print("[ERROR]: The length of the messages are not equal")
-        return
+        return "[ERROR]"
     
     blocks_len:int = len(M1)
     leter_in_M1:list = {}
     leter_in_M2:list = {}
     leter_in_M3:list = {}
+
     for i in range(blocks_len):
         if leter_in_M1.get(M1[i]) == None:
             leter_in_M1[M1[i]] = [i]
@@ -32,19 +33,21 @@ def find_the_special_block_cipher(M1, M2, M3):
                 if (leter_in_M1[M1[i]][0] == leter_in_M2[M2[i]][0] and
                     leter_in_M1[M1[i]][1] == leter_in_M2[M2[i]][1]):
                     print(f"[INFO]: The special block cipher is M3")
-                    return
+                    return "M3"
             elif len(leter_in_M3[M3[i]]) == 2:
                 if (leter_in_M1[M1[i]][0] == leter_in_M3[M3[i]][0] and
                     leter_in_M1[M1[i]][1] == leter_in_M3[M3[i]][1]):
                     print(f"[INFO]: The special block cipher is M2")
-                    return
+                    return "M2"
         elif len(leter_in_M2[M2[i]]) == 2:
             if len(leter_in_M3[M3[i]]) == 2:
                 if (leter_in_M2[M2[i]][0] == leter_in_M3[M3[i]][0] and
                     leter_in_M2[M2[i]][1] == leter_in_M3[M3[i]][1]):
                     print(f"[INFO]: The special block cipher is M1")
-                    return
+                    return "M1"
+                
     print("[ERROR]: There is no special block cipher")
+    return "[ERROR]"
 
 
 if __name__ == "__main__":
@@ -52,4 +55,5 @@ if __name__ == "__main__":
     M2 = "lzlgx ujprk uqqxt vnsne uffhj uztol"
     M3 = "sfzql oexpo bxaqe fpqbu qcolj qobkq"
 
-    find_the_block_cipher(M1, M2, M3)
+    block:str = find_the_special_block_cipher(M1, M2, M3)
+    # print(block)
