23d81a1764166da14f099caea13006149557b693
ShaharAshe <shaharas30@gmail.com>
2024-07-14 20:34:34 +0300

    add script for universal_exponent_algorithm

---
+++ fast_decrypt_algorithm.py
@@ -0,0 +1,31 @@
+def chinese_residue_theorem(p, q, n, a, b):
+    M_1 = p*(pow(p, -1, q))
+    M_2 = q*(pow(q, -1, p))
+    
+    x_1 = a%q
+    x_2 = a%p
+
+    d_1 = b%(q-1)
+    d_2 = b%(p-1)
+
+    a_1 = (x_1**d_1)%q
+    a_2 = (x_2**d_2)%p
+
+    c = (M_1*a_1 + M_2*a_2) % n
+    # print(f'{M_1 = }, {M_2 = }, {a_1 = }, {a_2 = }, {n = }')
+
+    return c
+
+
+if __name__ == "__main__":
+    # base^power mod n
+    n = 47197
+    p = 109
+    q = 433
+
+    base = 32975
+    power = 5489
+
+    res_c = chinese_residue_theorem(p, q, n, base, power)
+
+    print(f"Chinese Residue Theorem: {res_c} mod {n}.")
---
+++ universal_exponent_algorithm.py
@@ -0,0 +1,79 @@
+class UniversalExponent:
+    def __init__(self, n, d, e):
+        self.n = n
+        self.d = d
+        self.e = e
+        self.a = 2
+        self.ed_sub_1 = self.__factorize((d*e)-1)
+        
+        print("\n================")
+        print(f"we got:")
+        print(f"n = {self.n}")
+        print(f"d = {self.d}")
+        print(f"e = {self.e}")
+        print("================\n")
+
+        print(f"ed-1 = {(self.e*self.d)-1} = 2^{self.ed_sub_1[0]}*{self.ed_sub_1[1]} = 2^{self.ed_sub_1[0]}*r")
+        
+    def __gcd(self, num_1:int, num_2:int)->int:
+        if num_1 == 0:
+            return 0
+        return num_1 if num_2 == 0 else self.__gcd(num_2, num_1%num_2)
+    
+    def __find_a(self, n:int)->None:
+        self.a += 1
+        while True:
+            if (self.__gcd(self.a, n) != 1):
+                self.a += 1
+            else:
+                break
+
+    def __factorize(self, n:int) -> tuple[int, int]:
+        k:int = 0
+        while n % 2 == 0:
+            n //= 2
+            k += 1
+        return (k, n)
+    
+    def process(self):
+        while True:
+            print("_____________________________________________________")
+            print(f"a = {self.a}:")
+            print("---------------------------------")
+            pow_ex = pow(self.a, self.ed_sub_1[1], self.n)
+            print(f"{self.a}^r = {(2**self.ed_sub_1[0])%self.n} mod {self.n}")
+            for i in range(self.ed_sub_1[0]):
+                pow_ex_temp = pow(pow_ex, 2, self.n)
+                print(f"{pow_ex}^2 = {pow_ex_temp} mod {self.n}")
+                if pow_ex_temp == 1:
+                    break
+                elif pow_ex_temp == -1:
+                    print("[ERROR] - The algorithm failed because the number is not a prime number")
+                    print("_____________________________________________________")
+                    return
+                else:
+                    pow_ex = pow_ex_temp
+            print("---------------------------------")
+            if pow(pow_ex, 2, self.n) == 1:
+                decompose_1 = self.__gcd(pow_ex-1, self.n)
+                decompose_2 = 0
+                if decompose_1 != 0:
+                    decompose_2 = self.n//decompose_1
+                print(f"Decomposition of n: {self.n} = {decompose_1}*{decompose_2}")
+                print("_____________________________________________________")
+                return
+            else:
+                print(f"{self.a} is not a universal exponent lets try another number")
+                self.__find_a(self.n)
+                print(f"New a: {self.a}")
+                return
+
+
+if "__main__" == __name__:
+    n = 47197
+    d = 5489
+    e = 17
+
+    ue = UniversalExponent(n, d, e)
+    ue.process()
+    
