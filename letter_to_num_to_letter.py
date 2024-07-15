9b85aff8f3d32922cf55c34c6a19d8b85f632e01
ShaharAshe <shaharas30@gmail.com>
2024-07-15 14:38:00 +0300

    add calculate of letter to number and number to letter

---
+++ letter_to_num_to_letter.py
@@ -0,0 +1,17 @@
+def calc_char_to_num(char:str):
+    num = ord(char.lower())-ord("a")
+    print(f"{char} --> {num}")
+    return num
+
+def calc_num_to_char(num:int):
+    char:str = chr(num+ord("a"))
+    print(f"{num} --> {char}")
+    return char
+
+if __name__ == "__main__":
+    char = "a"
+    num = 14
+
+    calc_char_to_num(char=char)
+    calc_num_to_char(num=num)
+    
