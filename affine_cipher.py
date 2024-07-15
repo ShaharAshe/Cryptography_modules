4550ed5c64055dd7e065e90207d484a15fe0b110
ShaharAshe <shaharas30@gmail.com>
2024-07-15 13:11:45 +0300

    add affine cipher

---
+++ affine_cipher.py
@@ -0,0 +1,27 @@
+def decrypt(plaintext:str, a:int, b:int) -> str:
+    de_txt:list = []
+    a_1 = pow(a, -1, 26)
+    print(f"[cipher] - e(x) = {a}x + {b}")
+    print(f"[de-cipher] - d(x) = {a_1}(y - {b})")
+    print("--------------")
+    for c in plaintext:
+        if c.isalpha():
+            temp_c = (((ord(c.lower())-ord("a"))-b)*a_1)%26
+            print(f'e({c}) = {c}*{a} + {b} = {temp_c} mod 26')
+            temp_c = chr(temp_c+ord("a"))
+            de_txt.append(temp_c)
+    print("--------------")
+    for c in de_txt:
+        print(c, end=" ")
+    print()
+
+    return de_txt
+
+
+if __name__ == "__main__":
+    plaintext = "IGRANQH"
+    a = 11
+    b = 16
+
+    de_txt = decrypt(plaintext, a, b)
+    # print(de_txt)
--- affine_two_char.py
+++ affine_two_char.py
@@ -1,1 +1,87 @@
-def affi
+def affine(origin_1, origin_2, cipher_1, cipher_2):
+    a_co_1:int = (ord(origin_1.lower())-ord("a"))%26
+    b_co_1:int = 1
+    c_co_1:int = (ord(cipher_1.lower())-ord("a"))%26
+
+    a_co_2:int = (ord(origin_2.lower())-ord("a"))%26
+    b_co_2:int = 1
+    c_co_2:int = (ord(cipher_2.lower())-ord("a"))%26
+    
+    print(f"We have:")
+    print(f"e({origin_1}) = {cipher_1}")
+    print(f"{origin_1} = {a_co_1} // {origin_2} = {c_co_1}")
+    print("AND:")
+    print(f"e({origin_2}) = {cipher_2}")
+    print(f"{origin_2} = {a_co_2} // {cipher_2} = {c_co_2}", end="\n\n")
+
+    print("Now we gonna find the key for the affine cipher.")
+
+    a_co_1:int = (ord(origin_1.lower())-ord("a"))%26
+    b_co_1:int = 1
+    c_co_1:int = (ord(cipher_1.lower())-ord("a"))%26
+
+    a_co_2:int = (ord(origin_2.lower())-ord("a"))%26
+    b_co_2:int = 1
+    c_co_2:int = (ord(cipher_2.lower())-ord("a"))%26
+
+    a_co:int = (a_co_2-a_co_1)%26
+    b_co:int = (b_co_2-b_co_1)%26
+    c_co:int = (c_co_2-c_co_1)%26
+
+    a = -1
+    b = -1
+    if a_co_1 == 0:
+        b = c_co_1
+        a_co:int = a_co_2
+        b_co:int = b_co_1
+        c_co:int = c_co_2
+
+        print(f"[1] - {c_co_1}=({a_co_1})a+({b_co_1})b mod 26")
+        print(f"[2] - {c_co_2}=({a_co_2})a+({b_co_2})b mod 26")
+        print(f"Then we have:")
+        print(f"[3] - {c_co}=({a_co})a+({b_co})b = ({a_co})a+({b_co})({b}) mod 26")
+    elif a_co_2 == 0:
+        b = c_co_2
+        a_co:int = a_co_1
+        b_co:int = b_co_2
+        c_co:int = c_co_1
+
+        print(f"[1] - {c_co_1}=({a_co_1})a+({b_co_1})b mod 26")
+        print(f"[2] - {c_co_2}=({a_co_2})a+({b_co_2})b mod 26")
+        print(f"Then we have:")
+        print(f"[3] - {c_co}=({a_co})a+({b_co})b = ({a_co})a+({b_co})({b}) mod 26")
+    elif b_co_1 == 0:
+        a = a_co_1
+        a_co:int = a_co_2
+        b_co:int = b_co_1
+        c_co:int = c_co_2
+
+        print(f"[1] - {c_co_1}=({a_co_1})a+({b_co_1})b mod 26")
+        print(f"[2] - {c_co_2}=({a_co_2})a+({b_co_2})b mod 26")
+        print(f"Then we have:")
+        print(f"[3] - {c_co}=({a_co})a+({b_co})b = ({a_co})({a})+({b_co})b mod 26")
+    elif b_co_2 == 0:
+        a = a_co_2
+        a_co:int = a_co_1
+        b_co:int = b_co_2
+        c_co:int = c_co_1
+
+        print(f"[1] - {c_co_1}=({a_co_1})a+({b_co_1})b mod 26")
+        print(f"[2] - {c_co_2}=({a_co_2})a+({b_co_2})b mod 26")
+        print(f"Then we have:")
+        print(f"[3] - {c_co}=({a_co})a+({b_co})b = ({a_co})({a})+({b_co})b mod 26")
+    else:
+        print(f"[1] - {c_co_1}=({a_co_1})a+({b_co_1})b mod 26")
+        print(f"[2] - {c_co_2}=({a_co_2})a+({b_co_2})b mod 26")
+        print(f"Then we have:")
+        print(f"[3] - {c_co}=({a_co})a+({b_co})b mod 26")
+
+
+if __name__ == "__main__":
+    origin_1 = "E"
+    cipher_1 = "I"
+
+    origin_2 = "S"
+    cipher_2 = "G"
+
+    affine(origin_1, origin_2, cipher_1, cipher_2)
